8f3a3d0e98fbe92e97689422bbd3fa3da1256dd8
ren <kikuren1st@gmail.com>
2024-03-19 21:47:05 +0900

    route

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,30 +1,30 @@
 import { PrismaClient } from "@prisma/client"
-import { NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 const prisma=new PrismaClient()
 
 //todoリスト全取得API
-export const GET = async (_req: Request, res: NextApiResponse) => {
+export const GET = async (req: Request, res: NextResponse) => {
     try {
       await prisma.$connect();
       const posts = await prisma.post.findMany();
-      res.status(200).json({ message: "Success", posts });
+      return NextResponse.json({message:"Success",posts},{status:200});
     } catch (err) {
-      res.status(500).json({ message: "Error", err });
+      return NextResponse.json({message:"Error",err},{status:500});
     } finally {
       await prisma.$disconnect();
     }
   }
 
 //todoリスト投稿API
-  export const POST = async (req: Request, res: NextApiResponse) => {
+  export const POST = async (req: Request, res: NextResponse) => {
     try {
       const { title } = await req.json();
       await prisma.$connect();
       const post = await prisma.post.create({ data: { title } });
-      res.status(201).json({ message: "Success", post });
+      return NextResponse.json({message:"Success",post},{status:201});
     } catch (err) {
-      res.status(500).json({ message: "Error", err });
+      return NextResponse.json({message:"Error",err},{status:500});
     } finally {
       await prisma.$disconnect();
     }
