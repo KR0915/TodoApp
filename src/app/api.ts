1b551038330b8c8ac1f28cbca21059040d0529d5
ren <kikuren1st@gmail.com>
2024-03-19 16:12:50 +0900

    url correction

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,6 @@
 //全Todoリスト取得API
 export async function getAllTodos(){
-    const res=await fetch(`http://localhost:3000/api/todo`,{
+    const res=await fetch(`https://todo-e60lr7u63-ren-kikuchis-projects.vercel.app/todo`,{
         cache:"no-store",//SSR or CSR
     });
     const todos=await res.json()
@@ -10,7 +10,7 @@ export async function getAllTodos(){
 
   //Todoリスト投稿API
 export const addTodo=async(title:string|undefined)=>{
-    const res=await fetch(`http://localhost:3000/api/todo`,{
+    const res=await fetch(`https://todo-e60lr7u63-ren-kikuchis-projects.vercel.app/api/todo`,{
         method:"POST",
         headers:{
             "Content-type":"application/json",
@@ -25,7 +25,7 @@ export const editTodo=async(
     title:string |undefined,
     id:number
     )=>{
-    const res=await fetch(`http://localhost:3000/api/todo/${id}`,{
+    const res=await fetch(`https://todo-e60lr7u63-ren-kikuchis-projects.vercel.app/api/todo/${id}`,{
         method:"PUT",
         headers:{
             "Content-Type":"application/json",
@@ -37,7 +37,7 @@ export const editTodo=async(
 
 //編集中に編集前の文字を表示
 export const getTodoByID=async(id:number)=>{
-    const res=await fetch(`http://localhost:3000/api/todo/${id}`);
+    const res=await fetch(`https://todo-e60lr7u63-ren-kikuchis-projects.vercel.app/api/todo/${id}`);
     const data=await res.json();//受け取った
     console.log(data)
     return data.post;//json形式でresを返す
@@ -45,7 +45,7 @@ export const getTodoByID=async(id:number)=>{
 
 //Todo1リスト削除API
 export const deleteTodo=async(id:number)=>{
-    const res=await fetch(`http://localhost:3000/api/todo/${id}`,{
+    const res=await fetch(`https://todo-e60lr7u63-ren-kikuchis-projects.vercel.app/api/todo/${id}`,{
         method:"DELETE",
         headers:{
             "Content-type":"application/json",
